bec3f8066b1f47a1ae0ece216e2907c9b8ce6804
zlob1llo <zlob1llo90@gmail.com>
2024-08-07 16:19:50 +0300

    2st home

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,67 +1,71 @@
 function getArrayParams(...arr) {
-	let min = Infinity;
-	let max = -Infinity;
-	for (let i = 0; i < arr.length; i++) {
-			if (arr[i] > max) {
-				max = arr[i]
-			};
-			if (arr[i] < min) {
-				min = arr[i]
-			};
-		};
-	let avg = Number((arr.reduce((acc, val) => acc + val) / arr.length).toFixed(2));
-	return {
-		min: min,
-		max: max,
-		avg: avg
-	};
+  let min = Infinity
+  let max = -Infinity
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  let avg = Number(
+    (arr.reduce((acc, val) => acc + val) / arr.length).toFixed(2)
+  )
+  return {
+    min: min,
+    max: max,
+    avg: avg,
+  }
 }
 
-
 function summElementsWorker(...arr) {
-	return arr.reduce((acc, val) => acc + val, 0);	 
+  return arr.reduce((acc, val) => acc + val, 0)
 }
 
 function differenceMaxMinWorker(...arr) {
-	return arr.length !== 0 ? Math.max(...arr) - Math.min(...arr) : 0;
+  return arr.length !== 0 ? Math.max(...arr) - Math.min(...arr) : 0
 }
 
 function differenceEvenOddWorker(...arr) {
-  let evenElement = 0;
-  let oddElement = 0;
-	for (let i = 0; i < arr.length; i++) {
-			if (arr[i] % 2 === 0) {
-				evenElement = evenElement + arr[i]
-			} else {
-				OddElement = oddElement + arr[i]
-			};
-		};
-  
-  return evenElement - oddElement;
+  let evenElement = 0
+  let oddElement = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenElement = evenElement + arr[i]
+    } else {
+      oddElement = oddElement + arr[i]
+    }
+  }
+
+  return evenElement - oddElement
 }
 
 function averageEvenElementsWorker(...arr) {
-	let evenElement = [];
+  let evenElement = []
   if (arr.length === 0) {
     return 0
-  } else     
-  for (let i = 0; i < arr.length; i++) {
-			if (arr[i] % 2 === 0) {
-				evenElement.push(arr[i])
-			};
-		};
-  return Number((evenElement.reduce((acc, val) => acc + val , 0 ) / evenElement.length).toFixed(2));
-};
-
-
-function makeWork (arrOfArr, func) {
-let maxWorkerResult = func(...arr);   
-for (let i = 0; i < arrOfArr.length; i++) {
- for (let u = 0; u < arrOfArr[i].length; u++) {
-   if (arrOfArr[i][u] > maxWorkerResult) {
-       maxWorkerResult = arrOfArr[i][u]
+  } else
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        evenElement.push(arr[i])
+      }
     }
-  };
-};
-return maxWorkerResult;
-};
+  return Number(
+    (
+      evenElement.reduce((acc, val) => acc + val, 0) / evenElement.length
+    ).toFixed(2)
+  )
+}
+
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = func(...arr)
+  for (let i = 0; i < arrOfArr.length; i++) {
+    for (let u = 0; u < arrOfArr[i].length; u++) {
+      if (arrOfArr[i][u] > maxWorkerResult) {
+        maxWorkerResult = arrOfArr[i][u]
+      }
+    }
+  }
+  return maxWorkerResult
+}
